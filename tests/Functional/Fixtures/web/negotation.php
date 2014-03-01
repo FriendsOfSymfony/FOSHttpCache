364bcb25640b6a207923558aeb8d1b25903df3f7
David de Boer <david@ddeboer.nl>
2014-03-01 13:52:38 +0100

    Fix test on HHVM
    HHVM adds (default) charset to Content-Type header for HTML responses.
    
    Remove echo

--- tests/Functional/Fixtures/web/negotation.php
+++ tests/Functional/Fixtures/web/negotation.php
@@ -3,5 +3,3 @@ header('Cache-Control: max-age=3600');
 header(sprintf('Content-Type: %s', $_SERVER['HTTP_ACCEPT']));
 header('X-Cache-Debug: 1');
 header('Vary: Accept');
-
-echo $_SERVER['HTTP_ACCEPT'];
