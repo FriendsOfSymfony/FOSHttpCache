ee6707a710c3cc501cb405d15275efee0068d36c
David Buchmann <david.buchmann@liip.ch>
2014-03-09 16:51:35 +0100

    make phpdoc more specific

--- src/Invalidation/Method/PurgeInterface.php
+++ src/Invalidation/Method/PurgeInterface.php
@@ -6,7 +6,10 @@ use FOS\HttpCache\Invalidation\CacheProxyInterface;
 
 /**
  * An HTTP cache that supports invalidation by purging, that is, removing one
- * URL from the cache
+ * URL from the cache.
+ *
+ * Implementations should be configurable with a default host to be able to
+ * handle purge calls that do not contain a full URL but only a path.
  */
 interface PurgeInterface extends CacheProxyInterface
 {
@@ -16,7 +19,10 @@ interface PurgeInterface extends CacheProxyInterface
      * Purging a URL will remove the cache for the URL, including the query
      * string, with all its Vary variants.
      *
-     * @param string $url
+     * If the $url is just a path, the cache proxy class will add a default
+     * host name.
+     *
+     * @param string $url Path or URL to purge.
      *
      * @return $this
      */
--- src/Invalidation/Method/RefreshInterface.php
+++ src/Invalidation/Method/RefreshInterface.php
@@ -6,7 +6,10 @@ use FOS\HttpCache\Invalidation\CacheProxyInterface;
 
 /**
  * An HTTP cache that supports invalidation by refresh requests that force a
- * cache miss for one specific URL
+ * cache miss for one specific URL.
+ *
+ * Implementations should be configurable with a default host to be able to
+ * handle refresh calls that do not contain a full URL but only a path.
  */
 interface RefreshInterface extends CacheProxyInterface
 {
@@ -15,6 +18,9 @@ interface RefreshInterface extends CacheProxyInterface
      *
      * Refreshing a URL will generate a new cached response for the URL,
      * including the query string but excluding any Vary variants.
+     *
+     * If the $url is just a path, the cache proxy class will add a default
+     * host name.
      *
      * @param string $url     Path or URL to refresh.
      * @param array  $headers Extra HTTP headers to send to the caching proxy
