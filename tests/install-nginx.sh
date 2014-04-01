4df989cb75e3c1403d8e13b3df767e49b4846bbf
Simone Fumagalli <simone@iliveinperego.com>
2014-04-01 23:15:42 +0200

    New way to install and call NGINX

--- tests/install-nginx.sh
+++ tests/install-nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sudo apt-get install nginx build-essential libc6 libpcre3 libpcre3-dev
-libpcrecpp0 libssl0.9.8 libssl-dev zlib1g zlib1g-dev lsb-base
+sudo apt-get install build-essential libc6 libpcre3 libpcre3-dev libpcrecpp0 libssl0.9.8 libssl-dev zlib1g zlib1g-dev lsb-base
 cd /tmp/
 mkdir custom_nginx
 cd custom_nginx
+mkdir build
 wget http://nginx.org/download/nginx-1.4.6.tar.gz
 wget http://labs.frickle.com/files/ngx_cache_purge-2.1.tar.gz
 tar -xvf nginx-1.4.6.tar.gz
@@ -12,9 +12,11 @@ tar -xvf ngx_cache_purge-2.1.tar.gz
 cd nginx-1.4.6
 
 ./configure \
-	  --with-debug \
-	    --add-module=/tmp/custom_nginx/ngx_cache_purge-2.1
+    --with-debug \
+    --prefix=/tmp/custom_nginx/build/ \
+    --add-module=/tmp/custom_nginx/ngx_cache_purge-2.1
 
 /usr/bin/make
-sudo /usr/bin/make install
-sudo chmod -R 777 /usr/local/nginx
+/usr/bin/make install
+
+sudo ln -s /tmp/custom_nginx/build/sbin/nginx /usr/sbin/nginx
