176d35ab7f6572b7c3ca3a06eaef1712076ba13d
David de Boer <david@ddeboer.nl>
2014-05-16 09:31:58 +0200

    Add FOS license header (fix #57)

--- tests/Functional/Fixtures/web/user_context.php
+++ tests/Functional/Fixtures/web/user_context.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the FOSHttpCache package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 header('X-Cache-Debug: 1');
 
 if ('POST' == strtoupper($_SERVER['REQUEST_METHOD'])) {
