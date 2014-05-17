bf82f4d527363524d1b9dec5e331a4f5c0f43439
David de Boer <david@ddeboer.nl>
2014-05-17 09:08:13 +0200

    Fix HHVM on Travis by using FastCGI instead of built-in webserver

---
+++ tests/ci/install-apache-hhvm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# https://github.com/facebook/hhvm/wiki/fastcgi
+
+sudo apt-get install apache2 libapache2-mod-fastcgi
+sudo a2enmod rewrite actions fastcgi alias
+
+# Configure apache virtual hosts
+sudo cp -f tests/ci/travis-ci-apache-hhvm /etc/apache2/sites-available/default
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+sudo service apache2 restart
+
+# Run HHVM
+hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000
--- tests/ci/install-apache.sh
+++ tests/ci/install-apache.sh
@@ -8,6 +8,6 @@ sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 # configure apache virtual hosts
-sudo cp -f tests/travis-ci-apache /etc/apache2/sites-available/default
+sudo cp -f tests/ci/travis-ci-apache /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 sudo service apache2 restart
