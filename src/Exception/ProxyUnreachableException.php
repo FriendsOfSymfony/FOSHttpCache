b4eee37a03ac19329ac3bf69db0865a2b0f86f0f
David Buchmann <david.buchmann@liip.ch>
2014-07-16 15:29:01 +0200

    provide request information in exception mesage

--- src/Exception/ProxyUnreachableException.php
+++ src/Exception/ProxyUnreachableException.php
@@ -19,19 +19,25 @@ class ProxyUnreachableException extends \RuntimeException implements HttpCacheEx
 {
     /**
      * @param string     $host     The host name that was contacted.
-     * @param string     $message  The error message from guzzle.
-     * @param \Exception $previous The exception from guzzle.
+     * @param string     $message  The error message from the HTTP client.
+     * @param string     $details  Further details about the request that caused the error.
+     * @param \Exception $previous The exception from the HTTP client.
      *
      * @return ProxyUnreachableException
      */
-    public static function proxyUnreachable($host, $message, \Exception $previous = null)
+    public static function proxyUnreachable($host, $message, $details = '', \Exception $previous = null)
     {
+        $message = sprintf(
+            'Request to caching proxy at %s failed with message "%s"',
+            $host,
+            $message
+        );
+        if ($details) {
+            $message .= ". $details";
+        }
+
         return new ProxyUnreachableException(
-            sprintf(
-                'Request to caching proxy at %s failed with message "%s"',
-                $host,
-                $message
-            ),
+            $message,
             0,
             $previous
         );
