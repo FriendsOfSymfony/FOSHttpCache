bf5403794ec4b64abd2182a767a939ffa0a2cb1a
David de Boer <david@ddeboer.nl>
2015-05-09 09:55:34 +0200

    Don't show version number in title

--- doc/conf.py
+++ doc/conf.py
@@ -110,10 +110,11 @@ pygments_style = 'sphinx'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+# Don't show version in title
+html_title = "FOSHttpCache Documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = "FOSHttpCache"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
