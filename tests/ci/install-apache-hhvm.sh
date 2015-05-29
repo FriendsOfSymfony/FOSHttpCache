e8cdbff535d7a9e73e4c1ae3c96a5be666ab8f2b
David Buchmann <david.buchmann@liip.ch>
2015-05-29 15:40:27 +0200

    make travis talk

--- tests/ci/install-apache-hhvm.sh
+++ tests/ci/install-apache-hhvm.sh
@@ -9,6 +9,9 @@ sudo cp -f tests/ci/travis-ci-apache-hhvm /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 sudo service apache2 restart
 
+cat /etc/hhvm/php.ini
+echo "hhvm.log.always_log_unhandled_exceptions = true" >> /etc/hhvm/php.ini
+
 # Run HHVM
 hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000 -vServer.FixPathInfo=true
 
