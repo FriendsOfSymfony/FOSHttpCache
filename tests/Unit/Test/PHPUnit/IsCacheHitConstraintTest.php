a25974e47e234f34ccbcc13de29613efad8b5c8f
David Buchmann <david.buchmann@liip.ch>
2017-08-19 11:04:04 +0200

    work around missing ExpectationFailedException forward compatiblity

--- tests/Unit/Test/PHPUnit/IsCacheHitConstraintTest.php
+++ tests/Unit/Test/PHPUnit/IsCacheHitConstraintTest.php
@@ -13,6 +13,11 @@ namespace FOS\HttpCache\Tests\Unit\Test\PHPUnit;
 
 use FOS\HttpCache\Test\PHPUnit\IsCacheHitConstraint;
 
+// phpunit 5 has forward compatibility classes but missed this one
+if (!class_exists('\PHPUnit\Framework\ExpectationFailedException')) {
+    class_alias('\PHPUnit_Framework_ExpectationFailedException', '\PHPUnit\Framework\ExpectationFailedException');
+}
+
 class IsCacheHitConstraintTest extends AbstractCacheConstraintTest
 {
     /**
