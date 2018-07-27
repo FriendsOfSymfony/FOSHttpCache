6f6d8c1ebb1fc2cb2bef5c9b775bd06c9b1b3d43
Yanick Witschi <yanick.witschi@terminal42.ch>
2018-07-27 10:56:55 +0200

    Added a listener that allows cleaning up cache tags headers for the Symfony proxy

---
+++ src/SymfonyCache/CleanupCacheTagsListener.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the FOSHttpCache package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\HttpCache\SymfonyCache;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Listener that allows to cleanup the cache tags header so it's not exposed
+ * to the world.
+ *
+ * @author Yanick Witschi <yanick.witschi@terminal42.ch>
+ */
+class CleanupCacheTagsListener implements EventSubscriberInterface
+{
+    /**
+     * @var string
+     */
+    private $tagsHeader;
+
+    /**
+     * @param string $tagsHeader The header that is used for cache tags.
+     */
+    public function __construct($tagsHeader = PurgeTagsListener::DEFAULT_TAGS_HEADER)
+    {
+        $this->tagsHeader = $tagsHeader;
+    }
+
+    /**
+     * Remove the cache headers.
+     *
+     * @param CacheEvent $e
+     */
+    public function cleanResponse(CacheEvent $e)
+    {
+        if (null === ($response = $e->getResponse())) {
+            return;
+        }
+
+        $response->headers->remove($this->tagsHeader);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            Events::POST_HANDLE => 'cleanResponse',
+        ];
+    }
+}
---
+++ tests/Unit/SymfonyCache/CleanupCacheTagsListenerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the FOSHttpCache package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\HttpCache\Tests\Unit\SymfonyCache;
+
+use FOS\HttpCache\SymfonyCache\CacheEvent;
+use FOS\HttpCache\SymfonyCache\CacheInvalidation;
+use FOS\HttpCache\SymfonyCache\CleanupCacheTagsListener;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Yanick Witschi <yanick.witschi@terminal42.ch>
+ */
+class CleanupCacheTagsListenerTest extends TestCase
+{
+    public function testNoResponse()
+    {
+        $listener = new CleanupCacheTagsListener();
+        $listener->cleanResponse($this->createEvent());
+        $this->addToAssertionCount(1); // Nothing should happen, just asserting the "response is null" case
+    }
+
+    public function testResponseHeaderIsCleanedUp()
+    {
+        $response = new Response();
+        $response->headers->set('X-Cache-Tags', 'foo, bar');
+
+        $listener = new CleanupCacheTagsListener();
+        $listener->cleanResponse($this->createEvent($response));
+
+        $this->assertFalse($response->headers->has('X-Cache-Tags'));
+
+
+        $response = new Response();
+        $response->headers->set('Foobar', 'foo, bar');
+
+        $listener = new CleanupCacheTagsListener('Foobar');
+        $listener->cleanResponse($this->createEvent($response));
+
+        $this->assertFalse($response->headers->has('Foobar'));
+    }
+
+    private function createEvent(Response $response = null)
+    {
+        return new CacheEvent(
+            $this->createMock(CacheInvalidation::class),
+            new Request(),
+            $response
+        );
+    }
+}
