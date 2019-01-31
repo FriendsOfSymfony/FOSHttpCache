55dbb32d7902f35db246530e0b07396d08f1c692
Yanick Witschi <yanick.witschi@terminal42.ch>
2019-01-31 17:14:55 +0100

    Let's see again

--- tests/install-openlitespeed.sh
+++ tests/install-openlitespeed.sh
@@ -5,20 +5,17 @@ tar -zxvf openlitespeed-1.4.42.tgz
 cd openlitespeed
 
 # Ensure correct owners for travis
-sed -i s#SERVERROOT=/usr/local/lsws#SERVERROOT=/tmp/openlitespeed#g ols.conf
 sed -i s/OPENLSWS_USER=nobody/OPENLSWS_USER=travis/g ols.conf
 sed -i s/OPENLSWS_GROUP=nobody/OPENLSWS_GROUP=travis/g ols.conf
 
 sudo ./install.sh
 
 # Remove examples
-sudo rm -r /tmp/openlitespeed/conf/vhosts/Example
-sudo rm -r /tmp/openlitespeed/Example
+sudo rm -r /usr/local/lsws/conf/vhosts/Example
+sudo rm -r /usr/local/lsws/Example
 
 # Copy config
 cd $TRAVIS_BUILD_DIR/tests/ci/openlitespeed
-sudo cp ./httpd_config.conf /tmp/openlitespeed/conf/httpd_config.conf
-sudo mkdir /tmp/openlitespeed/conf/vhosts/foshttpcachetest
-sudo cp ./vhconf.conf /tmp/openlitespeed/conf/vhosts/foshttpcachetest/vhconf.conf
-
-sudo ln -s /tmp/openlitespeed/bin/lswsctrl /usr/sbin/lswsctrl
+sudo cp ./httpd_config.conf /usr/local/lsws/conf/httpd_config.conf
+sudo mkdir /usr/local/lsws/conf/vhosts/foshttpcachetest
+sudo cp ./vhconf.conf /usr/local/lsws/conf/vhosts/foshttpcachetest/vhconf.conf
