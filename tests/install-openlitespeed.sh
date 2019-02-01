d0725241d9ef81556cc551e190cb3c6dc4379e4a
Yanick Witschi <yanick.witschi@terminal42.ch>
2019-02-01 14:10:28 +0100

    Install using apt

--- tests/install-openlitespeed.sh
+++ tests/install-openlitespeed.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-wget https://openlitespeed.org/packages/openlitespeed-1.4.42.tgz
-tar -zxvf openlitespeed-1.4.42.tgz
-cd openlitespeed
-
-# Ensure correct owners for travis
-sed -i s/OPENLSWS_USER=nobody/OPENLSWS_USER=travis/g ols.conf
-sed -i s/OPENLSWS_GROUP=nobody/OPENLSWS_GROUP=travis/g ols.conf
-
-sudo ./install.sh
+wget -O - http://rpms.litespeedtech.com/debian/enable_lst_debain_repo.sh | bash
+apt-get -y install openlitespeed
 
 # Remove examples
 sudo rm -r /usr/local/lsws/conf/vhosts/Example
@@ -19,5 +12,3 @@ cd $TRAVIS_BUILD_DIR/tests/ci/openlitespeed
 sudo cp ./httpd_config.conf /usr/local/lsws/conf/httpd_config.conf
 sudo mkdir /usr/local/lsws/conf/vhosts/foshttpcachetest
 sudo cp ./vhconf.conf /usr/local/lsws/conf/vhosts/foshttpcachetest/vhconf.conf
-
-ls -la /usr/local/lsws
