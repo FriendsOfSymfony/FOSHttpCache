61199a2e5132e2038d246ea36752d0f2cdd4d96d
David Buchmann <david.buchmann@liip.ch>
2021-12-20 15:16:09 +0100

    install lts

--- .github/workflows/setup-varnish.sh
+++ .github/workflows/setup-varnish.sh
@@ -3,9 +3,8 @@
 set -e
 # Install Varnish
 echo "### Installing Varnish $VARNISH_VERSION ###"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 45DACFFDB8AFA6AA
-VARNISH_VERSION=$VARNISH_VERSION bash -c 'curl -L https://packagecloud.io/varnishcache/varnish${VARNISH_VERSION//./}/gpgkey | sudo apt-key add -'
-VARNISH_VERSION=$VARNISH_VERSION bash -c 'curl -L "https://packagecloud.io/install/repositories/varnishcache/varnish${VARNISH_VERSION//./}/config_file.list?os=ubuntu&dist=focal&source=script" | sudo tee -a /etc/apt/sources.list'
+VARNISH_VERSION=60lts bash -c 'curl -L https://packagecloud.io/varnishcache/varnish${VARNISH_VERSION//./}/gpgkey | sudo apt-key add -'
+VARNISH_VERSION=60lts bash -c 'curl -L "https://packagecloud.io/install/repositories/varnishcache/varnish${VARNISH_VERSION//./}/config_file.list?os=ubuntu&dist=focal&source=script" | sudo tee -a /etc/apt/sources.list'
 cat /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -t focal varnish
