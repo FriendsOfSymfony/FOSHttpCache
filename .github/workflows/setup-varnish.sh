291d9146857faaaa447fc0bd326c2e86e9e296c4
David Buchmann <david.buchmann@liip.ch>
2021-12-20 15:17:40 +0100

    install some key

--- .github/workflows/setup-varnish.sh
+++ .github/workflows/setup-varnish.sh
@@ -5,7 +5,7 @@ set -e
 echo "### Installing Varnish $VARNISH_VERSION ###"
 VARNISH_VERSION=60lts bash -c 'curl -L https://packagecloud.io/varnishcache/varnish${VARNISH_VERSION//./}/gpgkey | sudo apt-key add -'
 VARNISH_VERSION=60lts bash -c 'curl -L "https://packagecloud.io/install/repositories/varnishcache/varnish${VARNISH_VERSION//./}/config_file.list?os=ubuntu&dist=focal&source=script" | sudo tee -a /etc/apt/sources.list'
-cat /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3AEAFFBB82FBBA5F
 sudo apt-get update
 sudo apt-get install -t focal varnish
 if [ "$VARNISH_MODULES_VERSION" != "" ]; then sh "${GITHUB_WORKSPACE}/tests/install-varnish-modules.sh"; fi
