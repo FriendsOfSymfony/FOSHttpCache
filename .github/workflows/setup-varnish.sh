22e8a806208b8f4b250869f29d367cf121d8f5cd
David Buchmann <david.buchmann@liip.ch>
2021-12-20 14:35:04 +0100

    install public key

--- .github/workflows/setup-varnish.sh
+++ .github/workflows/setup-varnish.sh
@@ -3,6 +3,7 @@
 set -e
 # Install Varnish
 echo "### Installing Varnish $VARNISH_VERSION ###"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 45DACFFDB8AFA6AA
 VARNISH_VERSION=$VARNISH_VERSION bash -c 'curl -L https://packagecloud.io/varnishcache/varnish${VARNISH_VERSION//./}/gpgkey | sudo apt-key add -'
 VARNISH_VERSION=$VARNISH_VERSION bash -c 'curl -L "https://packagecloud.io/install/repositories/varnishcache/varnish${VARNISH_VERSION//./}/config_file.list?os=ubuntu&dist=focal&source=script" | sudo tee -a /etc/apt/sources.list'
 cat /etc/apt/sources.list
