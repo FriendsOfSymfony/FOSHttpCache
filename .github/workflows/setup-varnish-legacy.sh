4f8952bd27459038c50f187dfcd1aa590a2baf12
David Buchmann <david.buchmann@liip.ch>
2022-01-14 12:09:07 +0100

    install legacy varnish

--- .github/workflows/setup-varnish-legacy.sh
+++ .github/workflows/setup-varnish-legacy.sh
@@ -2,7 +2,20 @@
 
 set -e
 echo "### Installing Legacy Varnish $VARNISH_VERSION ###"
-VARNISH_VERSION=$VARNISH_VERSION bash -c 'curl -s https://packagecloud.io/install/repositories/varnishcache/varnish${VARNISH_VERSION//./}/script.deb.sh' | sudo bash
-# this is broken: we don't find the version. when not specifying the version, we end up with varnish 5 from the regular ubuntu distribution
-VARNISH_VERSION=$VARNISH_VERSION bash -c 'sudo apt-get install varnish=${VARNISH_VERSION}.*'
+sudo apt-get update
+sudo apt-get install debian-archive-keyring curl gnupg apt-transport-https
+VARNISH_VERSION=$VARNISH_VERSION bash -c 'curl -s -L https://packagecloud.io/varnishcache/varnish${VARNISH_VERSION//./}/gpgkey' | sudo apt-key add -
+# hardcode to use ubuntu trusty. that would be 14.04, but it is no longer supported on github actions.
+# the varnish 3 repository on packagecloud.io does not provide packages for newer ubuntu distributions
+# the trusty packages seem to work. YOLO
+VARNISH_VERSION=$VARNISH_VERSION bash -c 'echo deb https://packagecloud.io/varnishcache/varnish${VARNISH_VERSION//./}/ubuntu/ trusty main' | sudo tee /etc/apt/sources.list.d/varnishcache_varnish.list
+cat /etc/apt/sources.list.d/varnishcache_varnish.list
+sudo tee /etc/apt/preferences.d/varnishcache > /dev/null <<-EOF
+Package: varnish varnish-* hitch
+Pin: release o=packagecloud.io/varnishcache/*
+Pin-Priority: 1000
+EOF
+sudo apt-get update
+
+sudo apt-get install varnish
 if [ "$VARNISH_MODULES_VERSION" != "" ]; then sh "${GITHUB_WORKSPACE}/tests/install-varnish-modules-legacy.sh"; fi
