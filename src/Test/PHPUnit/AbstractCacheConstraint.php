74fb8670d2e92493d9eca582178d57991161797b
David Buchmann <david.buchmann@liip.ch>
2024-03-25 12:13:29 +0100

    remove obsolete legacy support no longer needed

--- src/Test/PHPUnit/AbstractCacheConstraint.php
+++ src/Test/PHPUnit/AbstractCacheConstraint.php
@@ -12,19 +12,56 @@
 namespace FOS\HttpCache\Test\PHPUnit;
 
 use PHPUnit\Framework\Constraint\Constraint;
+use Psr\Http\Message\ResponseInterface;
 
-if (class_exists('PHPUnit_Runner_Version') && version_compare(\PHPUnit_Runner_Version::id(), '6.0.0', '<')) {
-    /*
-     * Using an early return instead of a else does not work when using the PHPUnit phar due to some weird PHP behavior
-     * (the class gets defined without executing the code before it and so the definition is not properly conditional)
-     */
-    class_alias('FOS\HttpCache\Test\Legacy\PHPUnit\AbstractCacheConstraint', 'FOS\HttpCache\Test\PHPUnit\AbstractCacheConstraint');
-} else {
-    /**
-     * Abstract cache constraint.
-     */
-    abstract class AbstractCacheConstraint extends Constraint
+abstract class AbstractCacheConstraint extends Constraint
+{
+    protected string $header;
+
+    public function __construct(string $header = 'X-Cache')
     {
-        use AbstractCacheConstraintTrait;
+        $this->header = $header;
+    }
+
+    public function matches($other): bool
+    {
+        if (!$other instanceof ResponseInterface) {
+            throw new \InvalidArgumentException('compare must compare with '.ResponseInterface::class.' got '.get_debug_type($other));
+        }
+        if (!$other->hasHeader($this->header)) {
+            $message = sprintf(
+                'Response has no "%s" header. Configure your caching proxy '
+                .'to set the header with cache hit/miss status.',
+                $this->header
+            );
+            if (200 !== $other->getStatusCode()) {
+                $message .= sprintf("\nStatus code of response is %s.", $other->getStatusCode());
+            }
+
+            $message .= "\nThe response headers are:\n\n";
+
+            foreach ($other->getHeaders() as $name => $values) {
+                foreach ($values as $value) {
+                    $message .= $name.': '.$value."\n";
+                }
+            }
+
+            $body = $other->getBody();
+            $body->rewind();
+            $message .= sprintf("\nThe response body is:\n\n %s", $body->getContents());
+
+            throw new \RuntimeException($message);
+        }
+
+        return str_contains($other->getHeaderLine($this->header), $this->getValue());
+    }
+
+    public function failureDescription($other): string
+    {
+        return sprintf(
+            'response (with status code %s) %s',
+            $other->getStatusCode(),
+            $this->toString()
+        );
     }
 }
