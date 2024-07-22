bc3874c862fb127c073b7f78a89b582cb8917edc
Alexander Schranz <alexander@sulu.io>
2024-07-22 16:53:47 +0200

    Set CustomTtlListener cache life time

--- tests/Functional/Fixtures/Symfony/AppKernel.php
+++ tests/Functional/Fixtures/Symfony/AppKernel.php
@@ -31,18 +31,21 @@ class AppKernel implements HttpKernelInterface
             case '/cache':
                 $response = new Response(microtime(true));
                 $response->setCache(['max_age' => 3600, 'public' => true]);
+                $response->headers->set('X-Reverse-Proxy-TTL', '3600');
 
                 return $response;
             case '/tags':
                 $response = new Response(microtime(true));
                 $response->setCache(['max_age' => 3600, 'public' => true]);
                 $response->headers->set('X-Cache-Tags', 'tag1,tag2');
+                $response->headers->set('X-Reverse-Proxy-TTL', '3600');
 
                 return $response;
             case '/tags_multi_header':
                 $response = new Response(microtime(true));
                 $response->setCache(['max_age' => 3600, 'public' => true]);
                 $response->headers->set('X-Cache-Tags', ['tag1', 'tag2']);
+                $response->headers->set('X-Reverse-Proxy-TTL', '3600');
 
                 return $response;
             case '/negotiation':
@@ -50,6 +53,7 @@ class AppKernel implements HttpKernelInterface
                 $response->setCache(['max_age' => 3600, 'public' => true]);
                 $response->headers->set('Content-Type', $request->headers->get('Accept'));
                 $response->setVary('Accept');
+                $response->headers->set('X-Reverse-Proxy-TTL', '3600');
 
                 return $response;
         }
