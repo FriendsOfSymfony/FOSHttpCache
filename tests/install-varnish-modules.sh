7f649b08c9fb2460afedb406e3fbaf8eb42319da
David Buchmann <david.buchmann@liip.ch>
2025-06-17 10:11:54 +0200

    ubuntu 20.04 is discontinued on github. fix varnish build
    drop varnish modules on some legacy builds

--- tests/install-varnish-modules.sh
+++ tests/install-varnish-modules.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "### Installing Varnish Modules ${VARNISH_MODULES_VERSION} ###"
-sudo apt-get install build-essential automake varnish-dev python3-sphinx
+sudo apt-get install build-essential automake libvarnishapi-dev python3-sphinx
 
 curl -A "FOS Github" -o /tmp/varnish-modules.tar.gz -D - -L -s https://github.com/varnish/varnish-modules/archive/refs/tags/${VARNISH_MODULES_VERSION}.tar.gz
 tar zxpf /tmp/varnish-modules.tar.gz -C /tmp/
